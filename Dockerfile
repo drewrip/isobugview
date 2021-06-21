db1b9fc63dff5ccc2679e094f7fb5f05429a335a
drewrip <drew.ripberger@gmail.com>
2021-06-20 22:31:11 -0400

    specified pglast version

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY /guiserver .
 COPY /db-isolation .
 RUN apt-get -y update
 RUN apt-get -y install python pip
-RUN pip3 install django pyyaml pglast
+RUN pip install django pyyaml
+RUN pip install 'pglast==1.7' --force-reinstall
 EXPOSE 8000
 CMD python3 manage.py runserver 0.0.0.0:8000
--- guiserver/runisodiff.sh
+++ guiserver/runisodiff.sh
@@ -8,16 +8,14 @@ mkdir jobs/${1}/conf
 mkdir jobs/${1}/conf/txn
 mkdir jobs/${1}/conf/schema
 
-cp ./sql_process/pglast_sqlparser_0916.py jobs/${1}/pglast_sqlparser_0916.py
+cp ./sql_process/pglast_sqlparser.py jobs/${1}/pglast_sqlparser.py
 cp ./sql_process/log_parser.py jobs/${1}/log_parser.py
 cp checker jobs/${1}/.
 cd jobs/${1}
 
-ls -R
-
 echo "Starting Analysis"
 # Parse SQL log against schema
-python3 pglast_sqlparser_0916.py sql.log schema.csv ./
+python3 pglast_sqlparser.py sql.log schema.csv .
 
 # Generate configs
 python3 log_parser.py pglast_schema.csv.txt
@@ -27,7 +25,6 @@ mv schema.csv conf/schema/schema.csv
 echo "schema_file = conf/schema/schema.csv" >> conf/pglast_schema.conf
 
 echo "Finished Parsing"
-
 
 ./checker -f conf/pglast_schema.conf -p 1 -k 2 -n 2 -u ex -i rc -r 123456 -m n -j 15 -s b > running.txt
 
